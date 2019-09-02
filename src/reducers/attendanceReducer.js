0d8ccf75926bbcb76e4965f8299077bc735690b7
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2019-09-02 22:30:23 +0100

    format attendance reducer file

--- src/reducers/attendanceReducer.js
+++ src/reducers/attendanceReducer.js
@@ -9,20 +9,28 @@ const attendanceReducer = (state=initialState, action) => {
   switch (action.type) {
     case asyncActionName(ATTENDANCES).failure:
       return {...state, error: action.payload.status, errorMessage: action.payload.error}
+
     case asyncActionName(ATTENDANCES).loading:
       return {...state, loading: action.payload}
+
     case asyncActionName(ATTENDANCES).success:
       return {...state, attendances: action.payload.attendances}
+
     case asyncActionName(CLOCK_OUT).failure:
       return {...state, error: action.payload.status, errorMessage: action.payload.error}
+
     case asyncActionName(CLOCK_OUT).success:
       return {...state, attendance: action.payload.attendance}
+
     case asyncActionName(CLOCK_OUT).loading:
       return {...state, updating: action.payload}
+
     case asyncActionName(CLOCK_IN).failure:
       return {...state, error: action.payload.status, errorMessage: action.payload.error}
+
     case asyncActionName(CLOCK_IN).success:
       return {...state, attendance: action.payload.attendance, attendances: [...state.attendances, action.payload.attendance]}
+
     case asyncActionName(CLOCK_IN).loading:
       return {...state, loading: action.payload}
 
