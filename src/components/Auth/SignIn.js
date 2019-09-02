7f02c253e852de080a5f0d2be81c2c4fa2dc14e7
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2019-09-02 01:12:52 +0100

    authcontroller

---
+++ src/components/Auth/SignIn.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+
+const signIn = ({handleChange, handleLogin, onChangeMode}) => {
+  const handleClick = () => {
+    onChangeMode('sign-up');
+  }
+
+  return (
+    <div className='auth'>
+      <div className='auth-card'>
+        <div className="form-group">
+          <label> Email </label>
+          <input
+            name='email'
+            type='email'
+            className='form-control'
+            placeholder='Enter email'
+            onChange={handleChange}
+          />
+        </div>
+        <div className='form-group'>
+          <label> Password </label>
+          <input
+            name='password'
+            type='password'
+            className='form-control'
+            placeholder="Enter password"
+            onChange={handleChange}
+          />
+        </div>
+        <button className="btn btn-primary auth-btn" onClick={handleLogin}>Sign in</button>
+        <div className="form-text"> or <Link to="#" className="card-link" onClick={handleClick}>Sign up</Link></div>
+      </div>
+    </div>
+  )
+}
+
+export default signIn;
---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import SignInController from '../../components/Auth/SignIn';
+import SignUpController from '../../components/Auth/SignUp';
+
+class Auth extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mode: 'sign-in',
+      user: {
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+      }
+    }
+  }
+
+
+  handleChange = (event) => {
+    // event.preventDefault();
+    const field = event.target.name;
+    const user = this.state.user;
+    user[field] = event.target.value;
+  
+    this.setState({user})
+  }
+
+  handleSignUp = () => {
+    // event.preventDefault();
+    this.props.signupUser(this.state.user);
+  }
+
+  handleLogin = () => {
+    // event.preventDefault();
+    this.props.loginUser(this.state.user)
+  }
+
+  onChangeMode = (mode) => {
+    this.setState({mode});
+  }
+
+  renderComponent = () => {
+    if (this.state.mode === 'sign-in') {
+    return (
+      <SignInController
+        onChangeMode={this.onChangeMode}
+        handleChange={this.handleChange}
+        handleLogin={this.handleLogin}
+      />
+    );
+    } else {
+      return (
+      <SignUpController
+        onChangeMode={this.onChangeMode}
+        handleChange={this.handleChange}
+        handleSignUp={this.handleSignUp}
+      />
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className="container" style={{marginTop: '23vh'}}>
+        {this.renderComponent()}
+      </div>
+    );
+  }
+}
+
+export default Auth;
