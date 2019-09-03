8365b86de4f40ee55e84b16108bab349c87276f5
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2019-09-03 08:16:32 +0100

    remove redux logger

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-import {createLogger} from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import Aux from './hoc/Aux';
@@ -11,7 +10,7 @@ import rootReducer from './reducers';
 import AuthController from './containers/Auth/AuthController';
 import DashboardController from './containers/Dashboard/DashboardController'
 
-const store = createStore(rootReducer, applyMiddleware(thunk, createLogger()))
+const store = createStore(rootReducer, applyMiddleware(thunk))
 function App() {
   return (
     <Provider store={store}>
